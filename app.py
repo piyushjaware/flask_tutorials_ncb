f46d8231378cad7cfd01e7aeee7e6ba01c03e2b4
piyushjaware <piyush.jaware@hotmail.com>
2022-06-05 14:38:48 -0500

    Fix styling and save report

--- app.py
+++ app.py
@@ -4,44 +4,40 @@ app = Flask(__name__)
 
 
 class Report:
-    
+
     def __init__(self, place, time, description):
         self.place = place
         self.time = time
         self.description = description
-        
-    def __repr__(self) -> str:
-        return self.place + " | "+ self.time + " | "+ self.description
-        
 
-reports = []
+    def __repr__(self):
+        return self.place + " | " + self.time + " | " + self.description
+
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
 
 @app.route("/report", methods=['GET', 'POST'])
 def report():
     if request.method == 'GET':
         return render_template('report.html')
     else:
-        print("POST request received")
         place = request.form.get("place")
         time = request.form.get("time")
         description = request.form.get("description")
-        print(place, time, description)
         report = Report(place, time, description)
-        reports.append(report)
+        save_report(report)
         return render_template('success.html')
-        
-        
-        
-@app.route("/submitted-reports")
-def submitted_reports():
-    return str(reports)
-    
 
 
 @app.route("/success")
 def success():
     return render_template('success.html')
+
+
+def save_report(report):
+    file = open('reports.txt', 'a')
+    file.write(str(report) + "\n")
+    file.close()
